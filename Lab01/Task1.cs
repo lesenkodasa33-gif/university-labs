7bedbfdd85170ac6b93903fdd27c39ef4d1da383
lesenkodasa33-gif <lesenkodasa33@gmail.com>
2026-09-15 13:13:31 +0300

    Fix repo structure

---
+++ Lab01/Task1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Task1
+{
+    static void Main()
+    {
+        double weight = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        double bmi = weight / (height * height);
+
+        Console.WriteLine(bmi);
+    }
+}
