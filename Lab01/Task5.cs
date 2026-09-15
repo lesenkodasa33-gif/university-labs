53cf154b73830a40d9d8a263088aef4703e0ccfe
lesenkodasa33-gif <lesenkodasa33@gmail.com>
2026-09-15 23:35:27 +0300

    Task 5

--- Lab01/Task5.cs
+++ Lab01/Task5.cs
@@ -1,41 +1,26 @@
 ﻿using System;
-// Task 5 у Task5.cs
+
 namespace oop_course
 {
     public static class Task5
     {
         public static void Run()
         {
-            Console.Write("Введіть номер дня тижня (1-7): ");
             int day = int.Parse(Console.ReadLine()!);
 
-            switch (day)
+            string info = day switch
             {
-                case 1:
-                    Console.WriteLine("Понеділок 08:00–18:00");
-                    break;
-                case 2:
-                    Console.WriteLine("Вівторок 08:00–18:00");
-                    break;
-                case 3:
-                    Console.WriteLine("Середа 09:00–17:00");
-                    break;
-                case 4:
-                    Console.WriteLine("Четвер 08:00–18:00");
-                    break;
-                case 5:
-                    Console.WriteLine("П'ятниця 08:00–16:00");
-                    break;
-                case 6:
-                    Console.WriteLine("Субота 09:00–14:00");
-                    break;
-                case 7:
-                    Console.WriteLine("Неділя вихідний");
-                    break;
-                default:
-                    Console.WriteLine("Некоректний день");
-                    break;
-            }
+                1 => "Понеділок, 08:00–18:00",
+                2 => "Вівторок, 08:00–18:00",
+                3 => "Середа, 09:00–17:00",
+                4 => "Четвер, 08:00–18:00",
+                5 => "П'ятниця, 08:00–16:00",
+                6 => "Субота, 09:00–14:00",
+                7 => "Неділя — вихідний",
+                _ => "невідомий день"
+            };
+
+            Console.WriteLine($"День: {info}");
         }
     }
 }
